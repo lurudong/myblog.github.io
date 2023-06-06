7ecf2c34091842715af70f6a96b6e97ffc2d1e98
dong <44517714+lurudong@users.noreply.github.com>
2023-06-06 23:58:04 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:lurudong/myblog.github.io.git master:gh-pages
+git push -f git@github.com:lurudong/myblog.github.io.git master
 
 cd -
